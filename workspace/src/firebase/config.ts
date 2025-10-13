a202f6aa6231894cb697078c621b3b6b92f21647
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-10-13 19:30:02 +0000

    ainda esta apontando pro projeto errado conecte nosso projeto ao correto

---
+++ workspace/src/firebase/config.ts
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  "projectId": "barbercutbot",
+  "appId": "1:229266030377:web:314a53be7adc4ebde30858",
+  "apiKey": "AIzaSyDpq_iES-V8uzM3j1KMAmfefbvfR3PhU74",
+  "authDomain": "barbercutbot.firebaseapp.com",
+  "measurementId": "G-5G37Z2W3G5",
+  "storageBucket": "barbercutbot.appspot.com",
+  "messagingSenderId": "229266030377"
+};
