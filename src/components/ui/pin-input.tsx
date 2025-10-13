0f07d1f8056ef6206c50e47c6bbcac12de56bd9e
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-10-13 23:22:51 +0000

    studio-343774762:~/studio{main}$ npx tsc --noEmit src/app/(app)/dashboar

--- src/components/ui/pin-input.tsx
+++ src/components/ui/pin-input.tsx
@@ -2,7 +2,7 @@
 "use client"
 
 import * as React from "react"
-import { OTPInput, OTPInputContext } from "input-otp"
+import { OTPInput, OTPInputContext, OTPInputProps } from "input-otp"
 import { Dot } from "lucide-react"
 
 import { cn } from "@/lib/utils"
