119d3e0a14a4145362855773d3ff8c141203286e
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-10-13 19:31:46 +0000

    aqui esta apontando pro errado

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  "projectId": "barbercutbot",
+  "projectId": "studio-343774762-16da7",
   "appId": "1:229266030377:web:314a53be7adc4ebde30858",
   "apiKey": "AIzaSyDpq_iES-V8uzM3j1KMAmfefbvfR3PhU74",
   "authDomain": "barbercutbot.firebaseapp.com",
