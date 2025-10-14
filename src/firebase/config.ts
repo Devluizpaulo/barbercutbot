1187af6e83b82cb89bcb57cb3bb3a8949607ff3c
Devluizpaulo <136992113+Devluizpaulo@users.noreply.github.com>
2025-10-13 21:24:12 -0300

    okt

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:229266030377:web:314a53be7adc4ebde30858",
   "apiKey": "AIzaSyDpq_iES-V8uzM3j1KMAmfefbvfR3PhU74",
   "authDomain": "barbercutbot.firebaseapp.com",
+  "measurementId": "G-5G37Z2W3G5",
   "storageBucket": "barbercutbot.appspot.com",
   "messagingSenderId": "229266030377"
 };
