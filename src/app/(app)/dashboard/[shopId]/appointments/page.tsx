164ecb918e2714dc96a799f22bd16fcb501e694f
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-11-09 03:45:01 +0000

    aumnete a lardura dos cards e melhore a responsividade

--- src/app/(app)/dashboard/[shopId]/appointments/page.tsx
+++ src/app/(app)/dashboard/[shopId]/appointments/page.tsx
@@ -233,7 +233,7 @@ export default function AppointmentsPage() {
           </div>
         </div>
         
-        <div className="grid grid-cols-1 lg:grid-cols-[1fr_280px] gap-6 flex-1 min-h-0">
+        <div className="grid grid-cols-1 lg:grid-cols-[1fr_340px] gap-6 flex-1 min-h-0">
             <div className="flex-1 flex flex-col min-h-0">
                 <CalendarView 
                     appointments={appointments || []}
